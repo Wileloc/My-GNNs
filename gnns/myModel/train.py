e80880b4bc6cea2a12daf30525091ba30762f9d6
Wileloc <961424441@qq.com>
2022-05-18 12:40:09 +0000

    完成训练部分的代码

--- gnns/myModel/train.py
+++ gnns/myModel/train.py
@@ -1,8 +1,12 @@
 import torch
 import argparse
-from gnns.utils import set_random_seed, get_device, load_data, add_node_feat
+from gnns.utils import set_random_seed, get_device, load_data, add_node_feat, METRICS_STR, evaluate
 from gnns.myModel.model import MyModel
 import torch.optim as optim
+from dgl.dataloading import MultiLayerNeighborSampler, NodeDataLoader
+import warnings
+import tqdm
+import torch.nn.functional as F
 
 
 def train(args):
@@ -12,7 +16,10 @@ def train(args):
         load_data(args.dataset, device)
     # add_node_feat(g, 'pretrained', args.node_embed_path, True)
     add_node_feat(g, 'random')
-    # features = g.nodes[predict_ntype].data['feat']
+
+    sampler = MultiLayerNeighborSampler(list(range(args.neighbor_size, args.neighbor_size + args.num_layers)))
+    train_loader = NodeDataLoader(g, {predict_ntype: train_idx}, sampler, device=device, batch_size=args.batch_size)
+    loader = NodeDataLoader(g, {predict_ntype: g.nodes(predict_ntype)}, sampler, device=device, batch_size=args.batch_size)
 
     model = MyModel(
         {ntype: g.nodes[ntype].data['feat'].shape[1] for ntype in g.ntypes}, 
@@ -22,7 +29,30 @@ def train(args):
     scheduler = optim.lr_scheduler.CosineAnnealingLR(
         optimizer, T_max=len(loader) * args.epochs, eta_min=args.lr / 100
     )
-    pass
+    warnings.filterwarnings('ignore', 'Setting attributes on ParameterDict is not supported')
+    for epoch in range(args.epochs):
+        model.train()
+        losses = []
+        for input_nodes, output_nodes, blocks in tqdm(train_loader):
+            batch_logits = model(blocks, blocks[0].srcdata['feat'])
+            batch_labels = labels[output_nodes[predict_ntype]]
+            loss = F.cross_entropy(batch_logits, batch_labels)
+            losses.append(loss.item())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            torch.cuda.empty_cache()
+        print('Epoch {:d} | Loss {:.4f}'.format(epoch, sum(losses) / len(losses)))
+        if epoch % args.eval_every == 0 or epoch == args.epochs - 1:
+            print(METRICS_STR.format(*evaluate(
+                model, loader, g, labels, data.num_classes, predict_ntype,
+                train_idx, val_idx, test_idx, evaluator
+            )))
+    if args.save_path:
+        torch.save(model.cpu().state_dict(), args.save_path)
+        print('模型已保存到', args.save_path)
 
 
 def main():
