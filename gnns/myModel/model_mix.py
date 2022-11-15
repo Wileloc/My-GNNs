bbc7f3628110945be41c363e1a62a4d1f90ebf30
Wileloc <961424441@qq.com>
2022-11-15 21:29:09 +0800

    修改部分代码

--- gnns/myModel/model_mix.py
+++ gnns/myModel/model_mix.py
@@ -2,6 +2,53 @@ import torch
 import torch.nn as nn
 from dgl.nn.pytorch.conv import GATConv
 import dgl
+from dgl.utils import expand_as_pair
+import dgl.function as fn
+from dgl.ops import edge_softmax
+import torch.nn.functional as F
+
+
+class BiGraphGAT(nn.Module):
+
+    def __init__(self, out_dim, num_heads, attn_drop=0.0, negative_slope=0.01, activation=None):
+        super().__init__()
+        self.out_dim = out_dim
+        self.num_heads = num_heads
+        self.fc_src = nn.Linear(out_dim*num_heads, out_dim*num_heads)
+        self.fc_dst = nn.Linear(out_dim*num_heads, out_dim*num_heads)
+        self.attn_l = nn.Parameter(torch.FloatTensor(size=(num_heads, out_dim)))
+        self.attn_r = nn.Parameter(torch.FloatTensor(size=(num_heads, out_dim)))
+        self.feat_drop = nn.Dropout(attn_drop)
+        self.leaky_relu = nn.LeakyReLU(negative_slope)
+        self.activation = activation
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        gain = nn.init.calculate_gain('relu')
+        nn.init.xavier_normal_(self.fc_src.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_dst.weight, gain=gain)
+        nn.init.xavier_normal_(self.attn_l, gain=gain)
+        nn.init.xavier_normal_(self.attn_r, gain=gain)
+
+    def forward(self, g, feats):
+        with g.local_scope():
+            feat_src, feat_dst = expand_as_pair(feats, g)
+            feat_src = self.fc_src(self.feat_drop(feat_src)).view(-1, self.num_heads, self.out_dim)
+            feat_dst = self.fc_dst(self.feat_drop(feat_dst)).view(-1, self.num_heads, self.out_dim)
+            el = (feat_src * self.attn_l).sum(dim=-1, keepdim=True)  # (N_src, K, 1)
+            er = (feat_dst * self.attn_r).sum(dim=-1, keepdim=True)  # (N_dst, K, 1)
+            g.srcdata.update({'ft': feat_src, 'el': el})
+            g.dstdata['er'] = er
+            g.apply_edges(fn.u_add_v('el', 'er', 'e'))
+            e = self.leaky_relu(g.edata.pop('e'))
+            g.edata['a'] = edge_softmax(g, e)  # (E, K, 1)
+
+            # 消息传递
+            g.update_all(fn.u_mul_e('ft', 'a', 'm'), fn.sum('m', 'ft'))
+            ret = g.dstdata['ft'].view(-1, self.num_heads * self.out_dim)
+            if self.activation:
+                ret = self.activation(ret)
+            return ret
 
 
 class BiGraphContrastLayer(nn.Module):
@@ -44,10 +91,13 @@ class ContrastLayer(nn.Module):
     hidden_dim: 隐藏层维数
     etypes: 图的关系三元组(src, edge_type, dst)
     '''
-    def __init__(self, in_dim, hidden_dim, metapaths, predict_ntype, attn_drop):
+    def __init__(self, in_dim, hidden_dim, metapaths, predict_ntype, num_heads, attn_drop, negative_slope):
         super().__init__()
+        # self.bigraphs = nn.ModuleDict({
+        #     mp: BiGraphContrastLayer(in_dim, hidden_dim, mp.split('-')[0], attn_drop) for mp in metapaths
+        # })
         self.bigraphs = nn.ModuleDict({
-            mp: BiGraphContrastLayer(in_dim, hidden_dim, mp.split('-')[0], attn_drop) for mp in metapaths
+            mp: BiGraphGAT(hidden_dim // num_heads, num_heads, attn_drop, negative_slope, F.relu) for mp in metapaths
         })
         self.predict_ntype = predict_ntype
     
@@ -66,23 +116,28 @@ class ContrastLayer(nn.Module):
                 continue
             if dtype not in n_feat:
                 n_feat[dtype] = {dtype: feat[dtype][dtype][:g.num_dst_nodes(dtype)]}
-            if stype == dtype:
-                num_nodes_dict = {stype: g.num_src_nodes(stype)}
-            else:
-                num_nodes_dict = {stype: g.num_src_nodes(stype), dtype: g.num_dst_nodes(dtype)}
-            bigraph = dgl.heterograph({
-                (stype, etype, dtype): g.edges(etype=etype)}, 
-                num_nodes_dict=num_nodes_dict
-            )
+            # if stype == dtype:
+            #     num_nodes_dict = {stype: g.num_src_nodes(stype)}
+            # else:
+            #     num_nodes_dict = {stype: g.num_src_nodes(stype), dtype: g.num_dst_nodes(dtype)}
+            # bigraph = dgl.heterograph({
+            #     (stype, etype, dtype): g.edges(etype=etype)}, 
+            #     num_nodes_dict=num_nodes_dict
+            # )
+            bigraph = g[stype, etype, dtype]
             for s_etype in feat[stype]:
                 if len(s_etype.split('-')) - 1 != layer_idx: continue
                 if stype == dtype:
-                    bigraph.nodes[stype].data['h'] = feat[stype][s_etype]
+                    # bigraph.nodes[stype].data['h'] = feat[stype][s_etype]
+                    n_feat[dtype][dtype+"-"+s_etype] = self.bigraphs[dtype+"-"+s_etype](bigraph, \
+                        feat[stype][s_etype])[:g.num_dst_nodes(dtype)]
                 else:
-                    bigraph.nodes[stype].data['h'] = feat[stype][s_etype]
-                    bigraph.nodes[dtype].data['h'] = feat[dtype][dtype][:g.num_dst_nodes(dtype)]
+                    # bigraph.nodes[stype].data['h'] = feat[stype][s_etype]
+                    # bigraph.nodes[dtype].data['h'] = feat[dtype][dtype][:g.num_dst_nodes(dtype)]
+                    n_feat[dtype][dtype+"-"+s_etype] = self.bigraphs[dtype+"-"+s_etype](bigraph, \
+                        (feat[stype][s_etype], feat[dtype][dtype][:g.num_dst_nodes(dtype)]))[:g.num_dst_nodes(dtype)]
                 # 二分图目标节点的表示
-                n_feat[dtype][dtype+"-"+s_etype] = self.bigraphs[dtype+"-"+s_etype](bigraph)[:g.num_dst_nodes(dtype)]
+                # n_feat[dtype][dtype+"-"+s_etype] = self.bigraphs[dtype+"-"+s_etype](bigraph, feats)[:g.num_dst_nodes(dtype)]
 
         return n_feat
 
@@ -96,14 +151,19 @@ class MyModel(nn.Module):
     :attn_drop Attention Dropout概率
     '''
 
-    def __init__(self, in_dims, hidden_dim, out_dim, etypes, layer, predict_ntype, layer_metapaths, metapath_numbers, attn_drop=0.5) -> None:
+    def __init__(
+        self, in_dims, hidden_dim, out_dim, etypes, layer, predict_ntype, 
+        layer_metapaths, metapath_numbers, num_heads=8, attn_drop=0.5, negative_slope=0.2) -> None:
         super().__init__()
         self.predict_ntype = predict_ntype
         self.fc_in = nn.ModuleDict({
             ntype: nn.Linear(in_dim, hidden_dim) for ntype, in_dim in in_dims.items()
         })
+        # self.contrast = nn.ModuleList(
+        #     [ContrastLayer(hidden_dim, hidden_dim, mps, predict_ntype, attn_drop) for _, mps in layer_metapaths.items()
+        # ])
         self.contrast = nn.ModuleList(
-            [ContrastLayer(hidden_dim, hidden_dim, mps, predict_ntype, attn_drop) for _, mps in layer_metapaths.items()
+            [ContrastLayer(hidden_dim, hidden_dim, mps, predict_ntype, num_heads, attn_drop, negative_slope) for _, mps in layer_metapaths.items()
         ])
         self.transformer = nn.TransformerDecoderLayer(d_model=hidden_dim, nhead=8, batch_first=True, dropout=attn_drop)
         self.predict = nn.Linear(hidden_dim * metapath_numbers, out_dim)
@@ -121,30 +181,30 @@ class MyModel(nn.Module):
         :feat 节点特征Dict {ntype: Tensor(N, d_i)}
         return 节点表征Dict {ntype: Tensor(N, d_h)}
         '''
-        n_feat = {
+        feat = {
             dtype: {dtype: self.fc_in[dtype](feat[dtype])} for dtype in feat
         }
 
         predict_nodes_num = blocks[-1].num_dst_nodes(self.predict_ntype)
         h = {
-            self.predict_ntype: n_feat[self.predict_ntype][self.predict_ntype][:predict_nodes_num]
+            self.predict_ntype: feat[self.predict_ntype][self.predict_ntype][:predict_nodes_num]
         }
         for idx, (block, layer) in enumerate(zip(blocks, self.contrast)):
-            n_feat = layer(block, n_feat, idx, len(self.contrast))
-            for dtype in n_feat:
-                if dtype == self.predict_ntype: continue
-                remove = []
-                for path in n_feat[dtype]:
-                    if len(path.split('-')) - 2 < idx and len(path.split('-')) - 2 >= 0:
-                        remove.append(path)
-                for path in remove:
-                    n_feat[dtype].pop(path)
-            for path in n_feat[self.predict_ntype]:
+            feat = layer(block, feat, idx, len(self.contrast))
+            # for dtype in feat:
+            #     if dtype == self.predict_ntype: continue
+            #     remove = []
+            #     for path in feat[dtype]:
+            #         if len(path.split('-')) - 2 < idx and len(path.split('-')) - 2 >= 0:
+            #             remove.append(path)
+            #     for path in remove:
+            #         feat[dtype].pop(path)
+            for path in feat[self.predict_ntype]:
                 if len(path.split('-')) - 2 == idx:
-                    h[path] = n_feat[self.predict_ntype][path][:predict_nodes_num]
+                    h[path] = feat[self.predict_ntype][path][:predict_nodes_num]
 
-        tgt = torch.stack(list(h.values()), dim=1)
-        z = self.transformer(tgt, tgt)
+        z = torch.stack(list(h.values()), dim=1)
+        z = self.transformer(z, z)
         z = self.predict(z.reshape(z.size()[0], -1))
         return z
 
--- gnns/myModel/train_mix.py
+++ gnns/myModel/train_mix.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
 from dgl.dataloading import MultiLayerNeighborSampler, NodeDataLoader
-from gnns.myModel.model_mix_augment import MyModel
+from gnns.myModel.model_mix import MyModel
 from gnns.utils import (METRICS_STR, add_node_feat, calc_metrics, get_device,
                         load_data, set_random_seed, hg_metapaths, accuracy)
 
@@ -19,8 +19,8 @@ def train(args):
     device = get_device(args.device)
     data, g, _, labels, predict_ntype, train_idx, val_idx, test_idx, evaluator = \
         load_data(args.dataset, device, reverse_self=True)
-    # add_node_feat(g, 'pretrained', args.node_embed_path, True)
-    add_node_feat(g, 'random')
+    add_node_feat(g, 'pretrained', args.node_embed_path, True)
+    # add_node_feat(g, 'random')
 
     all_mps = hg_metapaths(g.to(torch.device('cpu')), predict_ntype, args.num_layers+1)
     layer_metapaths = {
@@ -40,10 +40,12 @@ def train(args):
         {ntype: g.nodes[ntype].data['feat'].shape[1] for ntype in g.ntypes}, 
         args.num_hidden, data.num_classes, g.canonical_etypes, args.num_layers, predict_ntype, layer_metapaths, mpnums, attn_drop=args.dropout
     ).to(args.device)
-    if args.load:
-        model.load_state_dict(torch.load('model_new/myModel_mix_n3_distinct_mp.pt'))
-        evaluate(model, loader, g, labels, data.num_classes, predict_ntype, train_idx, val_idx, test_idx, evaluator)
-        exit(0)
+    if args.load_path:
+        model.load_state_dict(torch.load(args.load_path, map_location=device))
+    # if args.load:
+    #     model.load_state_dict(torch.load('model_new/myModel_mix_n3_distinct_mp.pt'))
+    #     evaluate(model, loader, g, labels, data.num_classes, predict_ntype, train_idx, val_idx, test_idx, evaluator)
+    #     exit(0)
     optimizer = optim.AdamW(model.parameters(), eps=1e-6)
     scheduler = optim.lr_scheduler.OneCycleLR(
         optimizer, args.lr, epochs=args.epochs, steps_per_epoch=len(train_loader),
@@ -55,8 +57,9 @@ def train(args):
         losses = []
         for input_nodes, output_nodes, blocks in tqdm(train_loader):
             train_labels = labels[output_nodes[predict_ntype]]
-            train_logits, augment_labels = model(blocks, blocks[0].srcdata['feat'], train_labels)
-            train_labels = torch.cat((train_labels, augment_labels))
+            # train_logits, augment_labels = model(blocks, blocks[0].srcdata['feat'], train_labels)
+            # train_labels = torch.cat((train_labels, augment_labels))
+            train_logits = model(blocks, blocks[0].srcdata['feat'])
             loss = F.cross_entropy(train_logits, train_labels)
             losses.append(loss.item())
             
@@ -64,16 +67,16 @@ def train(args):
             loss.backward()
             optimizer.step()
             scheduler.step()
-            torch.cuda.empty_cache()
         print('Epoch {:d} | Loss {:.4f}'.format(epoch, sum(losses) / len(losses)))
         if epoch % args.eval_every == 0 or epoch == args.epochs - 1:
             print(METRICS_STR.format(*evaluate(
                 model, loader, g, labels, data.num_classes, predict_ntype,
                 train_idx, val_idx, test_idx, evaluator
             )))
+            torch.save(model.state_dict(), args.save_path)
 
     if args.save_path:
-        torch.save(model.cpu().state_dict(), args.save_path)
+        torch.save(model.state_dict(), args.save_path)
         print('模型已保存到', args.save_path)
 
 
@@ -84,24 +87,7 @@ def evaluate(model, loader, g, labels, num_classes, predict_ntype,
     logits = torch.zeros(g.num_nodes(predict_ntype), num_classes, device=train_idx.device)
     for input_nodes, output_nodes, blocks in loader:
         logits[output_nodes[predict_ntype]] = model(blocks, blocks[0].srcdata['feat'])
-    # predict = logits.detach().cpu().argmax(dim=1)
-    # labels = labels.cpu()
-    # total_nodes = torch.cat([train_idx, val_idx, test_idx])
-    # class_accs = []
-    # for i in range(num_classes):
-    #     nodes = total_nodes[labels == i+1]
-    #     degrees = [g.in_degrees(nodes, etype=e) for e in ['writes', 'cites', 'has_topic_rev']]
-    #     nodes_degrees = torch.stack(degrees, dim=1)
-    #     nodes_degrees = torch.sum(nodes_degrees, dim=1)
-    #     accs = {}
-    #     step = 10
-    #     for k in range(10):
-    #         idxes = nodes[nodes_degrees < (k+1)*step][nodes_degrees[nodes_degrees < (k+1)*step] >= k*step]
-    #         if not len(idxes): continue
-    #         accs[k] = accuracy(predict[idxes], labels[idxes])
-    #     class_accs.append(accs)
-    # d = pd.DataFrame(class_accs)
-    # d.to_excel('degrees.xlsx')
+        torch.cuda.empty_cache()
     return calc_metrics(logits, labels, train_idx, val_idx, test_idx, evaluator)
 
 
@@ -120,7 +106,7 @@ def main():
     parser.add_argument('--eval-every', type=int, default=10, help='每多少个epoch计算一次准确率')
     parser.add_argument('--save-path', help='模型保存路径')
     parser.add_argument('--node-embed-path', default='model/word2vec/ogbn-mag.model', help='预训练顶点嵌入路径')
-    parser.add_argument('--load', type=bool, default=False, help='是否加载模型')
+    parser.add_argument('--load-path', help='是否加载模型')
     args = parser.parse_args()
     print(args)
     train(args)
